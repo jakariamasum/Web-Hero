d1e58163d137d06e09670465701abc36d1368e78
jakariamasum <jmphy698@gmail.com>
2023-10-13 00:59:12 +0600

    navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,65 @@
+import { useEffect, useState } from 'react';
 import Button from '../Button/Button';
 import styles from './Navbar.module.css';
+import bar from '../../assets/bars.png'
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
+    useEffect(() => {
+        function handleResize() {
+            const isMobile = window.innerWidth <= 768;
+            setOpen(false);
+            setMobile(isMobile);
+        }
+
+        window.addEventListener('resize', handleResize);
+        handleResize();
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    const [mobile, setMobile] = useState(window.innerWidth <= 768);
     return (
         <div className={styles.container}>
             <nav>
                 <h1>Web Hero</h1>
-                <ul>
+                {
+                    open === false && mobile === true ? (
+                        <div style={{ backgroundColor: '#4759EE', borderRadius: '5px', padding: '0.5rem' }}>
+                            <img src={bar} alt="" style={{ width: '1.5rem', height: '1.5rem' }} onClick={() => setOpen(true)} />
+                        </div>
+                    ) : (
+                        <ul className={styles.navmenu}>
+                            <li onClick={() => setOpen(false)}>
+                                < a to='#' onClick={() => setOpen(false)}>Home</ a>
+                            </li>
+                            <li onClick={() => setOpen(false)}>
+                                < a to='#' onClick={() => setOpen(false)}>Course Details</ a>
+                            </li>
+                            <li onClick={() => setOpen(false)}>
+                                < a to='#' onClick={() => setOpen(false)}>About Us</ a>
+                            </li>
+                            <li onClick={() => setOpen(false)}>
+                                < a to='#' onClick={() => setOpen(false)}>Contact</ a>
+                            </li>
+                            <li>
+                            <div>
+                    <Button text={'Enroll Now'} />
+                </div>
+                            </li>
+                        </ul>
+                    )
+                }
+                {/* <ul>
                     <li>Home</li>
                     <li>Course details</li>
                     <li>About Us</li>
                     <li>Contact</li>
-                </ul>
-                <div>
-                    <Button text={'Enroll Now'}/>
-                </div>
+                </ul> */}
+                {/* <div>
+                    <Button text={'Enroll Now'} />
+                </div> */}
             </nav>
 
         </div>
